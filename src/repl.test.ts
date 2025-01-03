77099484ebffd8d133edf6fa966f73286ec200d8
jradziejewski <192462@student.ue.wroc.pl>
2025-01-03 21:44:47 +0100

    add cleaninput with test

---
+++ src/repl.test.ts
@@ -0,0 +1,28 @@
+import { cleanInput } from "./repl.ts";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+	{
+		input: "       hello  world     ",
+		expected: ["hello", "world"],
+	},
+	{
+		input: "",
+		expected: [],
+	},
+	{
+		input: "       hello   ",
+		expected: ["hello"],
+	},
+])(
+	"cleanInput($input)",
+	({ input, expected }) => {
+		test(`Expected: ${expected}`, () => {
+			const actual = cleanInput(input);
+			expect(actual).toHaveLength(expected.length);
+			for (const i in expected) {
+				expect(actual[i]).toBe(expected[i]);
+			}
+		});
+	},
+);
